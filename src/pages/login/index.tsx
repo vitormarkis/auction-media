09811dab73c6a0cc9f45a7c9c4058be4899a34c3
Vitor Markis <vitormarkis2369@gmail.com>
2023-03-20 16:56:47 -0300

    create login form

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,19 +2,16 @@ import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { SubmitHandler } from "react-hook-form/dist/types"
 
-interface RegisterUserFields {
-  name: string
+interface LoginUserFields {
   username: string
-  email: string
   password: string
-  repeated_password: string
 }
 
-const Register: React.FC = () => {
-  const [error, setError] = useState("As senhas não coincidem")
-  const { register, reset, handleSubmit } = useForm<RegisterUserFields>()
+const Login: React.FC = () => {
+  const [error, setError] = useState("Esse usuário não existe em nosso banco de dados")
+  const { register, reset, handleSubmit } = useForm<LoginUserFields>()
 
-  const submitHandler: SubmitHandler<RegisterUserFields> = async (formData) => {
+  const submitHandler: SubmitHandler<LoginUserFields> = async (formData) => {
     await new Promise((res) => setTimeout(res, 1000))
     console.log(formData)
   }
@@ -23,22 +20,16 @@ const Register: React.FC = () => {
     <div className="flex h-screen items-center justify-center bg-[#f7f7f7] text-neutral-700">
       <div className="w-[320px] rounded-md bg-white p-3 shadow-lg shadow-black/20" style={{ gridArea: "2 / 2 / 3 / 3" }}>
         <div className="mb-4">
-          <h1 className="text-2xl font-bold">Seja bem-vindo</h1>
-          <p className="text font-thin">Cadastre-se para poder continuar</p>
+          <h1 className="text-2xl font-bold">Bem-vindo de volta</h1>
+          <p className="text font-thin">Preencha seus dados para continuar</p>
         </div>
         <form className="flex flex-col" onSubmit={handleSubmit(submitHandler)}>
-          <label>Nome:</label>
-          <input className="mb-4 rounded-md border border-zinc-300 px-4 py-2" {...register("name")} type="text" placeholder="Pedro da Silva..." />
           <label>Nome de usuário:</label>
           <input className="mb-4 rounded-md border border-zinc-300 px-4 py-2" {...register("username")} type="text" placeholder="pedrodasilva" />
-          <label>E-mail:</label>
-          <input className="mb-4 rounded-md border border-zinc-300 px-4 py-2" {...register("email")} type="email" placeholder="pedrosilva@gmail.com" />
           <label>Senha:</label>
           <input className="mb-4 rounded-md border border-zinc-300 px-4 py-2" {...register("password")} type="password" placeholder="Insira sua senha aqui..." />
-          <label>Confirme sua senha:</label>
-          <input className="mb-4 rounded-md border border-zinc-300 px-4 py-2" {...register("repeated_password")} type="password" placeholder="Repita sua senha..." />
           <p className="mb-4">
-            Já possui uma conta? <span className="text-blue-500 underline">Entrar</span>
+            Ainda não possui uma conta? <span className="text-blue-500 underline">Registrar</span>
           </p>
           {error && (
             <p className="font-semibold text-red-600 text-center mb-2">{error}</p>
@@ -52,4 +43,4 @@ const Register: React.FC = () => {
   )
 }
 
-export default Register
+export default Login
