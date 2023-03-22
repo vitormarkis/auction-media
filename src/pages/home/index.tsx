d8253a46c37ebfd9f17b756d273152344360eb7e
Vitor Markis <vitormarkis2369@gmail.com>
2023-03-22 20:08:45 -0300

    add logout button

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,20 @@
-import { GetServerSideProps } from "next"
-import { getSession, useSession } from "next-auth/react"
+import { GetServerSideProps } from "next";
+import { getSession, signOut } from "next-auth/react";
 
-const Home: React.FC = ({ user }) => {
-  console.log(user)
-  const session = useSession()
+interface Props {
+  user: {
+    name: string;
+  email: string;
+  image: string
+  }
+}
 
+const Home: React.FC<Props> = ({ user }) => {
   return (
     <div>
       <h1>Seja bem-vindo de volta {user.name}</h1>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
+      <pre>{JSON.stringify({}, null, 2)}</pre>
+      <button onClick={() => signOut()} className="py-2 px-6 rounded-md border-2 border-black bg-red-500 text-white ">Sair</button>
     </div>
   )
 }
